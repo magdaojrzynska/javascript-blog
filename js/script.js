59a49d954bf5fb7f46f9476ce673d41b116f8b40
Magdalena Ojrzynska <magdalena.ojrzynska@gmail.com>
2024-04-15 22:31:47 +0200

    Compile css

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function titleClickHandler(event){
   console.log('Link was clicked!');
   console.log(event);
   
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
 const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -12,9 +12,9 @@ for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+  /* [DONE] remove class 'active' from all articles */
 
 const activeArticles = document.querySelectorAll('.post.active');
 
