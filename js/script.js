ae356486908b35b7ca9e37cdeabde03aac51c3dc
Magdalena Ojrzynska <magdalena.ojrzynska@gmail.com>
2024-04-17 21:48:54 +0200

    Get href atribute, Find article by selector, Add class to article

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function titleClickHandler(event){
+  event.preventDefault();
   const clickedElement = this;  
   console.log('Link was clicked!');
   
@@ -12,24 +13,34 @@ for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
 
   clickedElement.classList.add('active')
   console.log('clickedElement:', clickedElement);
 
   /* [DONE] remove class 'active' from all articles */
 
-const activeArticles = document.querySelectorAll('.post.active');
+  const activeArticles = document.querySelectorAll('.post.active');
 
-for(let activeArticle of activeArticles){
+  for(let activeArticle of activeArticles){
   activeArticle.classList.remove('active');
 }
 
-  /* get 'href' attribute from the clicked link */
+  /* [IN PROGRESS] get 'href' attribute from the clicked link */
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  const articleSelector = clickedElement.getAttribute("href");
+  console.log(articleSelector)
 
-  /* add class 'active' to the correct article */
+  /* [IN PROGRESS] find the correct article using the selector (value of 'href' attribute) */
+
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle)
+
+  /* [IN PROGRESS] add class 'active' to the correct article */
+
+  targetArticle.classList.add('active')
+  console.log('targetArticle:', targetArticle);
+
 }
 
 const links = document.querySelectorAll('.titles a');
