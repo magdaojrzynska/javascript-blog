c51c85980c2fa3e2a7fdf36afc9ded10558c32ab
Magdalena Ojrzynska <magdalena.ojrzynska@gmail.com>
2024-04-20 18:51:13 +0200

    Fix html file

--- js/script.js
+++ js/script.js
@@ -44,8 +44,68 @@ function titleClickHandler(event) {
 }
 
 const links = document.querySelectorAll('.titles a');
+console.log(links)
 
 for (let link of links) {
     link.addEventListener('click', titleClickHandler);
 
 }
+
+
+const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+function clearMessages() {
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+}
+
+function generateTitleLinks() {
+    const titleList = document.querySelector(optTitleListSelector);
+    /* IN PROGRESS remove contents of titleList */
+
+    clearMessages()
+
+    /* IN PROGRESS for each article */
+
+    const articles = document.querySelectorAll(optArticleSelector);
+
+    let html = '';
+
+    for (let article of articles) {
+        console.log(article)
+        /* NIE DZIAŁA get the article id */
+
+        const articleID = article.getAttribute("id");
+        console.log(articleID)
+
+        /* find the title element */
+
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+        /* get the title from the title element */
+
+        /* create HTML of the link */
+
+        const linkHTML = '<li><a href="#' + articleID + '"><span>' + articleTitle + '</span></a></li>';
+        console.log(linkHTML)
+
+        //titleList.innerHTML = titleList.innerHTML + linkHTML;
+
+        /* insert link into titleList */
+
+        //titleList.insertAdjacentHTML("afterend", linkHTML)
+
+        html = html + linkHTML;
+    }
+
+    titleList.innerHTML = html;
+
+
+
+    
+
+}
+
+generateTitleLinks();
